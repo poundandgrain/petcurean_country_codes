cf3497ce9a78a82bd9e1fe108ddaf229089ca6ca
Flynn O'Connor <fsquaredesign@gmail.com>
2017-03-24 11:32:17 -0700

    Updated rest end point to hit prod int site

--- country-code-command.php
+++ country-code-command.php
@@ -2,7 +2,7 @@
 class Country_Code_Command extends WP_CLI_Command {
 	function __invoke(){
 
-		$response = wp_remote_get('http://petcurean-i18n.poundandgrain.ca/wp-json/pecturean/v1/regions/');
+		$response = wp_remote_get('https://petcurean.international/wp-json/pecturean/v1/regions/');
 		if( is_array($response) ) {
 			$body = $response['body'];
 			$langs = json_decode( $body );
