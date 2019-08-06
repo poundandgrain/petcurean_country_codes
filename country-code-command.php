65fb1e168fbb6913067ad2091a58820a45794c54
Chris Ng <ngstigator@gmail.com>
2019-08-06 10:37:48 -0700

    use static json from international site instead of buggy REST endpoint

--- country-code-command.php
+++ country-code-command.php
@@ -1,7 +1,8 @@
 <?php
 class Country_Code_Command extends WP_CLI_Command {
 	function __invoke(){
-
+		// IMPORTANT: the following is problematic code that has been kept for reference only.
+		/*
 		// url of the international site is needed for the command to work. 
 		$response = wp_remote_get('https://petcurean.international/wp-json/pecturean/v1/regions/');
 		if( is_array($response) ) {
@@ -9,12 +10,18 @@ class Country_Code_Command extends WP_CLI_Command {
 			$langs = json_decode( $body );
 			update_option( 'int_country_codes', $langs );
 		}
+		*/
+
+		// convert remote json into array and populate option
+		$response = file_get_contents('https://petcurean.international/app/uploads/pet-i18n-countrycodes.json');
+		$arylangs = json_decode($response);
+
+		if (json_last_error() == JSON_ERROR_NONE) {
+			update_option( 'int_country_codes', $arylangs );
+		}
 
 		// Print a success message
 		WP_CLI::success( "Country Codes have been saved" );
-
 	}
-
 }
-
 WP_CLI::add_command( 'get_country_codes', 'Country_Code_Command' );
